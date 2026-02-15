27409275d4c970a5041827e9ed316eac6c875f7f
Anshul <anshulbhaskar50@gmail.com>
2026-02-15 23:35:48 +0530

    Service/Mapper/PatientResposneDTO

--- src/main/java/com/pm/patient_service/service/PatientService.java
+++ src/main/java/com/pm/patient_service/service/PatientService.java
@@ -1,5 +1,27 @@
 package com.pm.patient_service.service;
 
+
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.mapper.PatientMapper;
+import com.pm.patient_service.model.Patient;
+import com.pm.patient_service.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class PatientService {
+    private PatientRepository patientRepository;
+
+    //Dependency injection
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+        return patients.stream().map(PatientMapper::toDTO).toList();
+    }
 
 }
